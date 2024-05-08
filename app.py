87e25655aa82a9cd19e11cf0161a32b40d8817cd
wimdavies <william.johnston.davies@gmail.com>
2024-05-08 17:57:06 +0100

    Complete Application#run for exercise at end of unit

--- app.py
+++ app.py
@@ -1,27 +1,31 @@
-from lib.database_connection import DatabaseConnection
 from lib.artist_repository import ArtistRepository
 from lib.album_repository import AlbumRepository
+from lib.database_connection import DatabaseConnection
 
+class Application():
+  def __init__(self):
+    self._connection = DatabaseConnection()
+    self._connection.connect()
+    self._connection.seed("seeds/music_library.sql")
 
-# Connect to the database
-connection = DatabaseConnection()
-connection.connect()
+  def run(self):
+    artist_repository = ArtistRepository(self._connection)
+    artists = artist_repository.all()
+    album_repository = AlbumRepository(self._connection)
+    albums = album_repository.all()
 
-# Seed with some seed data
-connection.seed("seeds/music_library.sql")
+    print("Welcome to the music library manager!\n")
+    print("What would you like to do?\n 1 - List all albums\n 2 - List all artists\n")
+    user_selection = input("Enter your choice: ")
+    if user_selection == "1":
+        print("\nHere is the list of albums:")
+        for album in albums:
+            print(f" * {albums.index(album) + 1} — {album.title}")
+    elif user_selection == "2":
+        print("\nHere is the list of artists:")
+        for artist in artists:
+            print(f" * {artists.index(artist) + 1} — {artist.name}")
 
-# Retrieve all artists
-artist_repository = ArtistRepository(connection)
-artists = artist_repository.all()
-
-# List them out
-for artist in artists:
-    print(artist)
-
-# Retrieve all albums
-album_repository = AlbumRepository(connection)
-albums = album_repository.all()
-
-# List them out
-for album in albums:
-    print(album.__dict__)
+if __name__ == '__main__':
+    app = Application()
+    app.run()
