f3f563970e68c160b2496e4170d5d1faed5b3af6
wimdavies <william.johnston.davies@gmail.com>
2024-05-08 17:33:00 +0100

    Test-drive AlbumRepository#create and #delete

--- lib/album_repository.py
+++ lib/album_repository.py
@@ -20,3 +20,17 @@ class AlbumRepository:
         row = rows[0]
         album = Album(row["id"], row["title"], row["release_year"], row["artist_id"])
         return album
+
+    def create(self, album):
+        self._connection.execute(
+            "INSERT INTO albums (title, release_year, artist_id) VALUES (%s, %s, %s)",
+            [album.title, album.release_year, album.artist_id]
+        )
+        return None
+
+    def delete(self, album_id):
+        self._connection.execute(
+            "DELETE FROM albums WHERE id = %s",
+            [album_id]
+        )
+        return None
--- tests/test_album_repository.py
+++ tests/test_album_repository.py
@@ -1,3 +1,4 @@
+from lib.album import Album
 from lib.album_repository import AlbumRepository
 
 def test_get_all_records(db_connection):
@@ -12,3 +13,22 @@ def test_find_a_record(db_connection):
     repository = AlbumRepository(db_connection)
     album = repository.find(1)
     assert album.id == 1
+
+def test_create_a_record(db_connection):
+    db_connection.seed("seeds/music_library.sql") # Seed our database with some test data
+    repository = AlbumRepository(db_connection)
+    album = Album(None, "Evermore", 2020, 3)
+    assert repository.create(album) == None
+
+    albums = repository.all()
+    assert len(albums) == 13
+    assert albums[-1].title == "Evermore"
+
+def test_delete_a_record(db_connection):
+    db_connection.seed("seeds/music_library.sql") # Seed our database with some test data
+    repository = AlbumRepository(db_connection)
+    assert repository.delete(1) == None
+
+    albums = repository.all()
+    assert len(albums) == 11
+    assert albums[0].id == 2
